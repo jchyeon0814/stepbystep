029631854ee4f9e5106b697b8314b792e01ccb1b
jchyeon0814 <jchyeon0814@gmail.com>
2024-12-05 20:11:12 +0900

    사용자, 성취모델 생성

--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model): #사용자 테이블
+    user_id = models.CharField(max_length=40) ####필드 클래스 반환
+    user_nm = models.CharField(max_length=40)
+    create_dt = models.DateTimeField("date published")
+    
+class UserAchv(models.Model): #사용자 성취 테이블
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    achv_name = models.CharField(max_length=100)
+    achv_max_value = models.PositiveIntegerField(default=0)
+    achv_value = models.PositiveIntegerField(default=0)
+    create_dt = models.DateTimeField("date published")
+    update_dt = models.DateTimeField("date published")
