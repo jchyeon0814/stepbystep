33a10610095e5d788c736ddce28d2501ba0b1b1f
jchyeon0814 <jchyeon0814@gmail.com>
2024-12-09 12:49:19 +0900

    User, UserAchv 마이그레이션 적용

--- user/models.py
+++ user/models.py
@@ -1,9 +1,18 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 class User(models.Model): #사용자 테이블
     user_id = models.CharField(max_length=40) ####필드 클래스 반환
     user_nm = models.CharField(max_length=40)
     create_dt = models.DateTimeField("date published")
+    
+    def __str__(self):
+        return self.user_id + "(" + self.user_nm + ")"
+    
+    def was_published_recently(self):
+        return self.create_dt >= timezone.now() - datetime.timedelta(days=1)
     
 class UserAchv(models.Model): #사용자 성취 테이블
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -12,3 +21,7 @@ class UserAchv(models.Model): #사용자 성취 테이블
     achv_value = models.PositiveIntegerField(default=0)
     create_dt = models.DateTimeField("date published")
     update_dt = models.DateTimeField("date published")
+    
+    def __str__(self):
+        return self.user_id + "(" + self.achv_name + ")"
+
