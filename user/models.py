adc0604f2cb23e46b306996e6f0fc29851a86e00
jchyeon0814 <jchyeon0814@gmail.com>
2024-12-10 12:49:58 +0900

    임시저장

--- user/models.py
+++ user/models.py
@@ -4,9 +4,10 @@ from django.db import models
 from django.utils import timezone
 
 class User(models.Model): #사용자 테이블
-    user_id = models.CharField(max_length=40) ####필드 클래스 반환
-    user_nm = models.CharField(max_length=40)
-    create_dt = models.DateTimeField("date published")
+    seq = models.AutoField(primary_key=True)
+    user_id = models.CharField(verbose_name="사용자 아이디", max_length=40) ####필드 클래스 반환
+    user_nm = models.CharField(verbose_name="사용자명", max_length=40)
+    create_dt = models.DateTimeField(verbose_name="생성일", default=timezone.now)
     
     def __str__(self):
         return self.user_id + "(" + self.user_nm + ")"
@@ -15,13 +16,13 @@ class User(models.Model): #사용자 테이블
         return self.create_dt >= timezone.now() - datetime.timedelta(days=1)
     
 class UserAchv(models.Model): #사용자 성취 테이블
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
-    achv_name = models.CharField(max_length=100)
-    achv_max_value = models.PositiveIntegerField(default=0)
-    achv_value = models.PositiveIntegerField(default=0)
-    create_dt = models.DateTimeField("date published")
-    update_dt = models.DateTimeField("date published")
+     = models.ForeignKey(User, verbose_name="사용자 아이디", on_delete=models.CASCADE)
+    achv_name = models.CharField(verbose_name="목표명", max_length=100)
+    achv_max_value = models.PositiveIntegerField(verbose_name="목표값", default=0)
+    achv_value = models.PositiveIntegerField(verbose_name="현재값", default=0)
+    create_dt = models.DateTimeField(verbose_name="생성일", default=timezone.now)
+    update_dt = models.DateTimeField(verbose_name="수정일", null=True, blank=True)
     
     def __str__(self):
-        return self.user_id + "(" + self.achv_name + ")"
+        return str(self.user.user_id) + "(" + self.achv_name + ")"
 
