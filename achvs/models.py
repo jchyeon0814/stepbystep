c1f0f4b93b89f0f8b26f8a16d35db2fdcedf1193
jchyeon0814 <jchyeon0814@gmail.com>
2024-12-13 12:48:16 +0900

    achvs 테이블 user_id 왜래키 변경

--- achvs/models.py
+++ achvs/models.py
@@ -6,7 +6,7 @@ from users.models import User
 
 # Create your models here.
 class UserAchv(models.Model): #사용자 성취 테이블
-    user_seq = models.ForeignKey(User, verbose_name="사용자키", on_delete=models.CASCADE)
+    user_ref = models.ForeignKey(User, verbose_name="사용자키", on_delete=models.CASCADE)
     achv_name = models.CharField(verbose_name="목표명", max_length=100)
     achv_max_value = models.PositiveIntegerField(verbose_name="목표값", default=0)
     achv_value = models.PositiveIntegerField(verbose_name="현재값", default=0)
